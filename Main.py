d8a71fe7492e0f825d9957e6b165ad4eb1f01c55
santiagoTabares197 <79674160+santiagoTabares197@users.noreply.github.com>
2024-04-26 20:32:01 -0500

    Creacion de usuario y validacion de credenciales

---
+++ Main.py
@@ -0,0 +1,16 @@
+from .ModuloUsuario import Usuario
+print("***BIENVENIDO A SCB ACCESORIOS***")
+print("Ingreso de sesion.")
+nombre = input("Ingresa tu usuario: ")
+pswrd = input("Ingresa tu contraseña: ")
+
+user = Usuario("santi",1234)
+
+
+
+
+
+
+
+
+
---
+++ ModuloUsuario.py
@@ -0,0 +1,14 @@
+
+class Usuario:
+    def __init__(self,nombre,passwrd):
+        self.nombre = nombre
+        self.passwrd = passwrd
+
+    def login(self):
+       if (self.nombre == "admin" and self.passwrd == 123):
+           pass
+       else:
+           pass
+    
+
+
